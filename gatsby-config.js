a5709f529877aeaef7698ddc23bf70fc9897ca88
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-02 19:54:47 +0100

    Fetch markdown data & inject into projects page

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `${__dirname}/src/pages/projects/`,
+        path: `${__dirname}/src/projects/`,
       },
     },
   ],

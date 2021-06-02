42e07439acac1ff3fc0a3497bb23156921eb4403
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-02 20:46:58 +0100

    Optimise images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout.js"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import Img from "gatsby-image"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+
   return (
     <Layout>
       <section className={styles.header}>
@@ -11,12 +14,24 @@ export default function Home() {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
-          <Link to="/projects" className={styles.btn}>
+          <Link className={styles.btn} to="/projects">
             My Portfolio Projects
           </Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
